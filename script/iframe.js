2b5c3a4025e5ab15539682695d79fa8a9f685868
nbubna <nathan@esha.com>
2014-05-05 14:22:15 -0700

    WIP

--- script/iframe.js
+++ script/iframe.js
@@ -1,35 +1,34 @@
 (function(D) {
-	var timeout;
-	function elementsSelected(elements) {
-		if (timeout){ clearTimeout(timeout); }
-		if (!elements.forEach){ elements = [elements]; }
-		timeout = setTimeout(function() {
-			elements.forEach(highlight);
-			setTimeout(function(){ elements.forEach(unhighlight); }, 1000)
-		}, 100);
-	};
-	function highlight(el){ el.setAttribute('_highlight', 'true'); }
-	function unhighlight(el){ el.removeAttribute('_highlight'); }
+    var timeout;
+    function elementsSelected(elements) {
+        if (timeout){ clearTimeout(timeout); }
+        var arr = elements.toArray();
+        timeout = setTimeout(function() {
+            arr.forEach(highlight);
+            setTimeout(function(){ arr.forEach(unhighlight); }, 1000)
+        }, 100);
+    };
+    function highlight(el){ el.setAttribute('_highlight', 'true'); }
+    function unhighlight(el){ el.removeAttribute('_highlight'); }
 
-	D.addEventListener("DOMContentLoaded", function() {
-		// modify iframe's DOMx list to handle the demo content
-		var add = D._.List.prototype.add;
-		Object.defineProperty(D._.List.prototype, 'add', { value: function() {
-			var ret = add.apply(this, arguments);
-			elementsSelected(ret);
-			return ret;
-		}});
+    D.addEventListener("DOMContentLoaded", function() {
+        // modify iframe's DOMx list to handle the demo content
+        var add = D._.List.prototype.add;
+        Object.defineProperty(D._.List.prototype, 'add', { value: function() {
+            var ret = add.apply(this, arguments);
+            elementsSelected(ret);
+            return ret;
+        }});
 
-		var shadowDom = D.getElementById("shadow-dom"),
-			textDom = D.getElementById("text-dom"),
-			stringify = D._.stringify.print,
-			update = function() {
-				var root = shadowDom.children[0],
-					html = stringify(root, true, '');
-				textDom.innerHTML = html;
-			};
-		HTML._children.body = [shadowDom];
-		shadowDom.addEventListener("DOMSubtreeModified", update);
-		update();
-	});
+        var shadowDom = D.query("#shadow-dom"),
+            textDom = D.query("#text-dom"),
+            stringify = D._.stringify.print,
+            update = function() {
+                var root = shadowDom.children[0],
+                    html = stringify(root, true, '');
+                textDom.innerHTML = html;
+            };
+        shadowDom.addEventListener("DOMSubtreeModified", update);
+        update();
+    });
 })(document);
